4a37767b0b07f6fb6c9fca7fa9d5a08c6f8984fa
Tian You Wang <tian.you.w@outlook.com>
2018-07-15 03:22:38 -0400

    Add room db

--- app/src/main/java/espertolabs/esperto_ble_watch/SummaryActivity.java
+++ app/src/main/java/espertolabs/esperto_ble_watch/SummaryActivity.java
@@ -1,6 +1,9 @@
 package espertolabs.esperto_ble_watch;
 
 import android.Manifest;
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.content.BroadcastReceiver;
@@ -211,9 +214,59 @@ public class SummaryActivity extends AppCompatActivity implements Observer {
             }
         }
     }
+
     public void greetUser(){
         String greetText = "Welcome " + user.getName() + "!";
         messageUser.setText(greetText);
+    }
+
+    @Entity(tableName = "user")
+    public class User {
+
+        @PrimaryKey(autoGenerate = true)
+        private int uid;
+
+        @ColumnInfo(name = "first_name")
+        private String firstName;
+
+        @ColumnInfo(name = "last_name")
+        private String lastName;
+
+        @ColumnInfo(name = "age")
+        private int age;
+
+        public int getUid() {
+            return uid;
+        }
+
+        public void setUid(int uid) {
+            this.uid = uid;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
     }
 
     //TODO:: add a loading screen to keep user occupied before data display
